79e0589c3300686f53bf7f54bf1461ed8adfb732
Paulina <60793511+MFPaulis@users.noreply.github.com>
2020-03-07 16:18:28 +0100

    Add key - door communication

---
+++ Labirynt/Assets/Scripts/CollectMe.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectMe : MonoBehaviour
+{
+    public int count;
+    public GameObject manager;
+   
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        //Debug.Log("taken");
+        manager.GetComponent<CollectingItems>().ItemCollected(count);
+        Destroy(gameObject);
+    }
+}
